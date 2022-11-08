90bfe9942db1fe301ad1e746991e3769034f5c7b
g00400519 <g00400519@atu.ie>
2022-11-08 14:06:05 +0000

    File Week 7 transfer

--- src/main/java/ie/atu/Week7/UsingTryCatch.java
+++ src/main/java/ie/atu/Week7/UsingTryCatch.java
@@ -2,23 +2,24 @@ package ie.atu.Week7;
 
 import java.util.Scanner;
 
-public class UsingTryCatch
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Please enter a number between 0 - 9 :");
+public class UsingTryCatch {
+    public static void main(String[] args) {
+        //System.out.println("Please enter a number between 0 - 9 :");
         Scanner myscan = new Scanner(System.in);
-        try {
-            int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum >= 0) {
-                System.out.println("You entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
+        while (true) {
+            System.out.println("Please enter a number between 0 - 9 :");
+            try {
+                int myNum = myscan.nextInt();
+                if (myNum <= 9 && myNum >= 0) {
+                    System.out.println("You entered " + myNum);
+                } else {
+                    System.out.println("Not a valid number");
+                }
+            } catch (java.util.InputMismatchException e) {
+                System.out.println("Invalid Entry");
+                myscan.next();
+                continue;
             }
-        } catch(java.util.InputMismatchException e)
-        {
-            System.out.println("Invalid Entry");
         }
     }
-
 }
